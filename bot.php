fdb50df1172fa0a0bd734982677e548339d7ec67
Sunu Ilham Pradika <sunudika@gmail.com>
2020-04-23 08:01:59 +0700

    delete unused file or code

--- bot.php
+++ bot.php
@@ -30,16 +30,8 @@ $botman->hears("/start", function (BotMan $bot) {
     $bot->reply("Willkommen 😊");
 });
 
-$botman->hears("/idr_markets", function (BotMan $bot) {
-    global $IDR_MARKETS;
-    $idrMarkets = new Markets($IDR_MARKETS);
-    $bot->reply($idrMarkets->getResponses());
-});
-
-$botman->hears("/btc_markets", function (BotMan $bot) {
-    global $BTC_MARKETS;
-    $btcMarkets = new Markets($BTC_MARKETS);
-    $bot->reply($btcMarkets->getResponses());
+$botman->hears("/help", function (BotMan $bot) {
+    $bot->reply("bot ini mencatat frekuensi chat setiap user" . PHP_EOL . "/start");
 });
 
 $botman->hears("{coin}", function (BotMan $bot, $coin){
@@ -64,39 +56,5 @@ $botman->hears("{coin}", function (BotMan $bot, $coin){
         $bot->reply("Saya tidak kenal maksud anda");
     }
 });;
-
-// Message parameter
-$botman->hears("nama saya {nama}", function (BotMan $bot, $nama) {
-    $bot->reply("Salam kenal $nama, saya temennya bot anjaymabar");
-});
-
-// Fallback (balasan invalid command)
-$botman->fallback(function (BotMan $bot) {
-    $message = $bot->getMessage()->getText();
-    $bot->reply("Invalid command for '$message'");
-});
-
-// Regex, input salah = masuk fallback
-$botman->hears("request nilai ([0-9]+) di matkul (API|RPL)", function (BotMan $bot, $qty, $item) {
-    $bot->reply("Yomann, ntar matkul $item mu dapet $qty");
-});
-
-// Send image
-$botman->hears("/berangkat_ke_upn", function (BotMan $bot) {
-    $attachment = new Image("https://s.kaskus.id/r540x540/images/2014/03/23/6590730_20140323042249.png");
-
-    $message = OutgoingMessage::create("Nenek moyang menggunakannya bermil-mil jauhnya")->withAttachment($attachment);
-
-    $bot->reply($message);
-});
-
-// Send video
-$botman->hears("kirim video", function (BotMan $bot) {
-    $attachment = new Video("https://www.w3schools.com/html/mov_bbb.mp4");
-
-    $message = OutgoingMessage::create("Sample video")->withAttachment($attachment);
-
-    $bot->reply($message);
-});
 
 $botman->listen();

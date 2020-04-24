0ac471d90acbb0ea60f20c05df3c71f32099d607
Sunu Ilham Pradika <sunudika@gmail.com>
2020-04-24 12:28:52 +0700

    checkpoint

--- bot.php
+++ bot.php
@@ -4,11 +4,10 @@ use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
 use BotMan\Drivers\Telegram\TelegramDriver;
-use BotMan\BotMan\Messages\Attachments\Image;
-use BotMan\BotMan\Messages\Attachments\Video;
-use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 
-require_once "vendor/autoload.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bot-telegram-counter/vendor/autoload.php';
+
+date_default_timezone_set('Asia/Jakarta');
 
 $configs = [
     "telegram" => [
@@ -16,58 +15,49 @@ $configs = [
     ]
 ];
 
+require_once 'database/config.php';
+
+
 DriverManager::loadDriver(TelegramDriver::class);
 
 $botman = BotManFactory::create($configs);
 
-$botman->group(['recipient' => ['-1001307666764', '-1001184380882']], function(Botman $bot) {
+// $botman->group(['recipient' => ['-1001307666764', '-1001184380882']], function(Botman $bot) {
 
-    $bot->hears("/start", function (BotMan $bot) {
-        $user = $bot->getUser();
-        $firstname = $user->getFirstName();
-        $bot->reply("Willkommen $firstname 😊");
-    });
+//     $bot->hears("/start", function (BotMan $bot) {
+//         $user = $bot->getUser();
+//         $firstname = $user->getFirstName();
+//         $bot->reply("Willkommen $firstname 😊");
+//     });
 
-    $bot->hears("/kenal {nama}, {npm}", function (BotMan $bot, $nama, $npm) {
-        $bot->reply("Halo! $nama, $npm");
-    });
+//     $bot->hears("/kenal {nama}, {npm}", function (BotMan $bot, $nama, $npm) {
+//         $bot->reply("Halo! $nama, $npm");
+//     });
 
-    $bot->hears("/help", function (BotMan $bot) {
-        $bot->reply("bot ini mencatat frekuensi chat setiap user" . PHP_EOL . "/start - untuk mendapat sapaan");
-    });
-});
+//     $bot->hears("/help", function (BotMan $bot) {
+//         $bot->reply("bot ini mencatat frekuensi chat setiap user" . PHP_EOL . "/start - untuk mendapat sapaan");
+//     });
+// });
 
 $botman->hears("/start", function (BotMan $bot) {
     $user = $bot->getUser();
     $firstname = $user->getFirstName();
-    $bot->reply("Willkommen $firstname 😊");
+    $id_user = $user->getId();
+    $bot->reply("Willkommen $firstname ($id_user) 😊");
 });
 
 $botman->hears("/kenal {nama}, {npm}", function (BotMan $bot, $nama, $npm) {
     $bot->reply("Halo! $nama, $npm");
 });
 
-// $botman->hears("{coin}", function (BotMan $bot, $coin){
-//     global $coin_idr_markets;
-//     global $coin_btc_markets;
-//     $many = strlen($coin);
-//     $temp = substr($coin, 1, $many);
-
-//     if(in_array($temp, $coin_idr_markets)) {
-//         $coinIDR = new CoinIDR($temp);
-//         $bot->reply($coinIDR->getResponses());
-//     } else if(in_array($temp, $coin_btc_markets)) {
-//         $coinBTC = new CoinBTC($temp);
-//         $bot->reply($coinBTC->getResponses());
-//     } else if ($coin === "/btc_markets"){
-
-//     } else if ($coin === "/idr_markets"){
-
-//     } else if ($coin === "/start"){
-
-//     } else {
-//         $bot->reply("Saya tidak kenal maksud anda");
-//     }
-// });;
+$botman->hears("{chat}", function (BotMan $bot, $chat){
+    // $bot->typesAndWaits(4);
+    include "functions/request.php"; 
+    $message = "IDmu: " . $dataUser->id_user . PHP_EOL;
+    $message .= "Usernamemu: " . $dataUser->username . PHP_EOL;
+    $message .= "Namamu: " . $dataUser->name . PHP_EOL;
+    $message .= "Frekuensimu: " . $dataUser->frequency . PHP_EOL;
+    $bot->reply($message);
+});
 
 $botman->listen();
